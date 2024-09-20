8f5dd66eec4b969f0bcfacb7b689b494bd9fa650
Max <139768623+zerotul777@users.noreply.github.com>
2024-09-21 02:13:10 +0300

    add cv, tg

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const swiper = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-	// autoplay: {
-	//   delay: 5000,
-	// },
-	// effect: 'cube',
-	// cubeEffect: {
-	//   slideShadows: false,
-	// },
+	autoplay: {
+		delay: 5000,
+	},
+	effect: 'cube',
+	cubeEffect: {
+		slideShadows: false,
+	},
 });
 
 // message-form
